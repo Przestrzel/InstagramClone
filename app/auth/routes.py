c29caeea0f6f80adb2ee4ed9e10f03ae0ab9d89d
Przestrzel <marek.przestrzelski00@gmail.com>
2022-07-06 16:06:03 +0200

    Provide logging and registering users

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,14 +1,20 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, current_user
+from app import db
 from app.auth import bp
 from app.auth.forms import LoginForm, RegisterForm
+from app.models import User
 
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        print(form)
-
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('auth.login'))
+        login_user(user)
+        flash("You have successfully logged in!")
     return render_template('auth/login.html', form=form)
 
 
@@ -16,6 +22,12 @@ def login():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        print(form)
-
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None:
+            user = User(username=form.username.data, email=form.email.data)
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            flash("You have successfully registered!")
+            return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from app import db
+from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -9,8 +9,13 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
-    def set_password(self, password):
+    def set_password(self, password: str):
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
+    def check_password(self, password: str):
         return check_password_hash(self.password_hash, password)
+
+    @staticmethod
+    @login.user_loader
+    def load_user(id: str):
+        return User.query.get(int(id))
