6c88b04e0521c3f480710e96bc5c68c2bcec25b3
Przestrzel <marek.przestrzelski00@gmail.com>
2022-07-06 15:37:41 +0200

    Create migrations

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
 bootstrap = Bootstrap()
+login = LoginManager()
 
 
 def create_app(config=Config):
@@ -15,6 +17,7 @@ def create_app(config=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login.init_app(app)
     bootstrap.init_app(app)
 
     from app.errors import bp as errors_bp
