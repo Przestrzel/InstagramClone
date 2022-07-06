81355aee9f26293fe9ce6d39b50794d2066a7dab
Przestrzel <marek.przestrzelski00@gmail.com>
2022-07-06 15:16:26 +0200

    Form for registration

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,21 @@
 from flask import render_template
 from app.auth import bp
+from app.auth.forms import LoginForm, RegisterForm
 
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form)
+
+    return render_template('auth/login.html', form=form)
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        print(form)
+
+    return render_template('auth/register.html', form=form)
